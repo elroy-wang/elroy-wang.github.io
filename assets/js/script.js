5f602e4a6428dd34efbe624b4e1813b92f309bdc
elroy-wang <elroy7777777@gmail.com>
2023-09-04 23:29:28 +0800

    主页修改

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,10 +81,10 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
+
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
